35186ff8412105cd523274708ff7866bdda037bb
hibit-at <hibit_at@hotmail.com>
2021-07-24 10:17:16 +0900

    dp

--- dp.py
+++ dp.py
@@ -1,19 +1,18 @@
-import copy
 import os
 import pickle
-from collections import defaultdict, deque
+from collections import defaultdict
 
 import numpy as np
 
 # hyper_parameter
 
-agari_num = 14
-pai_range = 21
+agari_num = 11
+pai_range = 27
 
 manzu = '一二三四五六七八九'
+pinzu = '①②③④⑤⑥⑦⑧⑨'
 souzu = '１２３４５６７８９'
-jihai = '白発中'
-all_hai = manzu + souzu + jihai
+all_hai = manzu + pinzu + souzu
 
 tab = {}
 cnt = 0
@@ -51,12 +50,13 @@ def dfs_agari(v):
         maisu += v[i]
         if v[i] > 4:
             return
-    if maisu == agari_num:
+    if maisu % 3 == 2:
         w = tuple(v)
         if not agari[w]:      
-            if len(agari) % 10000 == 0:
+            if len(agari) % 1000 == 0:
                 print(f'{len(agari)} 番目のアガり形は {decode(v)}')
         agari[w] = True
+    if maisu == agari_num:
         return
     if maisu == 0:
         for i in range(pai_range):
@@ -65,94 +65,90 @@ def dfs_agari(v):
             dfs_agari(w)
     if maisu > 1:
         for i in range(pai_range):
-            if v[i] > 1:
-                continue
             w = v.copy()
             w[i] += 3
             dfs_agari(w)
         for i in range(7):
-            if v[i] > 3 or v[i+1] > 3 or v[i+2] > 3:
-                continue
             w = v.copy()
             w[i] += 1
             w[i+1] += 1
             w[i+2] += 1
             dfs_agari(w)
-            if v[i+9] > 3 or v[i+10] > 3 or v[i+11] > 3:
-                continue
             w = v.copy()
             w[i+9] += 1
             w[i+10] += 1
             w[i+11] += 1
             dfs_agari(w)
+            w = v.copy()
+            w[i+18] += 1
+            w[i+19] += 1
+            w[i+20] += 1
+            dfs_agari(w)
     return
 
 
 def make_graph():
-    global graph
-    global sep_graph
+    
+    visited_graph = {}
     shanten = -.5
 
-    # アガリを末端の goal として設定。これを目指すような DAG を形成。
-    for a in agari:
-        sep_graph[shanten][a] = []
-        graph[a] = []
+    if os.path.exists('graph_-0.5.pcl'):
+        current_graph = pickle.load(open('graph_-0.5.pcl','rb'))
+    else:
+        current_graph = defaultdict(list)
+        # アガリを末端の goal として設定。これを目指すような dp table を形成。
+        for a in agari:
+            current_graph[a] = []
+        pickle.dump(current_graph,open('graph_-0.5.pcl','wb'))
+
     cnt = 1
 
-    while sep_graph[shanten]:
+    while os.path.exists(f'graph_{shanten}.pcl'):
 
-        # 何切る
+        if os.path.exists(f'graph_{shanten+.5}.pcl'):
+            print(f'{shanten} シャンテンは既に計算が済んでいるようです')
+            shanten += .5
+            continue
 
-        print(f'{shanten} シャンテンの何切る')
+        if shanten == -.5:
+            visited_graph = {}
+        else:
+            visited_graph = pickle.load(open(f'graph_{shanten-.5}.pcl','rb'))
+        current_graph = pickle.load(open(f'graph_{shanten}.pcl','rb'))
+        new_graph = defaultdict(list)
 
-        for goal in sep_graph[shanten]:
+        if shanten % 1 == .5:
+            state = '何切る'
+            nanikiru = True
+        else:
+            state = '何待ち'
+            nanikiru = False
+
+        for goal in current_graph:
             if cnt % 10000 == 0:
-                print(f'{decode(goal)} は {cnt} 番目のノードです')
+                print(f'{decode(goal)} は {cnt} 番目のノードです。（{shanten} シャンテンの{state}）')
             cnt += 1
             for i in range(pai_range):
-                # 持っていない牌は切り牌になりえない
-                if goal[i] == 0:
-                    continue
-                start = list(goal)
-                start[i] -= 1
-                start = tuple(start)
-                # アガリだった場合、問答無用で辺を張る
-                if shanten == -.5:
-                    sep_graph[shanten+.5][start].append(goal)
-                    graph[start].append(goal)
-                    continue
-                # 自分のgoalだったものは対象としない（つまり、必ず向聴数が増えるノードに辺を張らせる）
-                if start in sep_graph[shanten-.5]:
-                    continue
-                sep_graph[shanten+.5][start].append(goal)
-                graph[start].append(goal)
-                # ノードから辺が張られたのが初めてだった場合、それを探索キューに追加
-
-        # 何待ち
-
-        shanten += .5
-
-        print(f'{shanten} シャンテンの何待ち')
-
-        for goal in sep_graph[shanten]:
-            if cnt % 10000 == 0:
-                print(f'{decode(goal)} は {cnt} 番目のノードです')
-            cnt += 1
-            for i in range(pai_range):
-                # 4枚ある牌は待ち牌になりえない
-                if goal[i] > 3:
-                    continue
-                start = list(goal)
-                start[i] += 1
+                if nanikiru:
+                    # 持っていない牌は切り牌になりえない
+                    if goal[i] == 0:
+                        continue
+                    start = list(goal)
+                    start[i] -= 1
+                else:
+                    # 4枚ある牌は待ち牌になりえない
+                    if goal[i] > 3:
+                        continue
+                    start = list(goal)
+                    start[i] += 1
                 start = tuple(start)
                 # 自分のgoalだったものは対象としない（つまり、必ず向聴数が増えるノードに辺を張らせる）
-                if start == (1, 3, 3, 1, 0, 0, 0, 0, 0):
-                    print('stop')
-                if start in sep_graph[shanten-.5]:
+                if start in visited_graph:
                     continue
-                sep_graph[shanten+.5][start].append(goal)
-                graph[start].append(goal)
-
+                new_graph[start].append(goal)
+        
+        if len(new_graph) > 0:
+            pickle.dump(new_graph,open(f'graph_{shanten+.5}.pcl','wb'))
         shanten += .5
 
 
@@ -164,29 +160,6 @@ else:
     dfs_agari(v)
     pickle.dump(agari, open('agari.pcl', 'wb'))
 
+make_graph()
 
-def func():
-    return defaultdict(list)
-
-
-if os.path.exists('sep_graph.pcl') and os.path.exists('graph.pcl'):
-    sep_graph = pickle.load(open('sep_graph.pcl', 'rb'))
-    graph = pickle.load(open('graph.pcl', 'rb'))
-else:
-    sep_graph = defaultdict(func)
-    graph = defaultdict(list)
-    make_graph()
-    pickle.dump(sep_graph, open('sep_graph.pcl', 'wb'))
-    pickle.dump(graph, open('graph.pcl', 'wb'))
-
-richi = sep_graph[0]
-
-richi = sorted(richi.items(), key = lambda x : -len(x[1]))
-richi = richi[:5]
-
-for r in richi:
-    print(decode(r[0]),'の待ち牌',len(r[1]),'種類')
-    machihai = [decode(m) for m in r[1]]
-    print(','.join(machihai))
-    
 print('stop')
